b83c4c9f198ebe6618b40d4be73bb5e4ef54ed00
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-12-06 18:14:35 +0900

    use %Net.JSON.JWT

--- init.db/00_security.sh
+++ init.db/00_security.sh
@@ -4,7 +4,8 @@ zn "%sys"
 set p("Enabled") = 0
 do ##class(Security.Users).Modify("_SYSTEM",.p)
 do ##class(Security.Users).Modify("Admin",.p)
-do ##class(Security.Users).Modify("SuperUser",.p)
+#;do ##class(Security.Users).Modify("SuperUser",.p)
+set sc=##class(Security.Users).UnExpireUserPasswords("*", .count) 
 kill p
 set p("NameSpace") = "CONDUIT"
 do ##class(Security.Users).Modify("irisowner",.p)
